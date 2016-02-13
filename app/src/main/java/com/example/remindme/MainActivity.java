43adc8876ed8325aaf3e7a5dc65d4902af4f5e8c
YuriChernigin <juriichernigin@gmail.com>
2016-02-14 02:02:38 +0300

    Create MainActivity

---
+++ app/src/main/java/com/example/remindme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by www_c on 14.02.2016.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
